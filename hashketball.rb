2477951ede807106417d92edbcebb4de78c53e59
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-13 23:09:35 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,7 +118,7 @@ end
 def good_practices
   game_hash.each do |location, team_data|
     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-    
+    binding.pry
       team_data.each do |attribute, data|
         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
         binding.pry
@@ -130,7 +130,7 @@ def good_practices
     end
   end
 end
-good_practices
+
 
 
 
