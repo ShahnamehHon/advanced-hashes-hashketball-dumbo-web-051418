16cd1938edf1e7a316cb2c8c76d28a282894f83e
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-13 22:33:27 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
   {
-    :home => {:team_name => {}, :colors => {}, :players => {}},
-    :away => {}
+    :home => {:team_name => {""}, :colors => {[]}, :players => {}},
+    :away => {:team_name => {""}, :colors => {[]}, :players => {}}
   }
 
 
